28f71ebc717d2f5bb7198866659ac1823a52f007
kerfiguetteb <kerfiguetteboutman@gmail.com>
2023-06-19 16:56:35 +0200

    feat:ajout de la fonctionnaliter de suppression par visibiliter

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,28 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/profile')]
 class AccountController extends AbstractController
 {
-    #[Route('/profile', name: 'app_account')]
+    #[Route('', name: 'app_account')]
     public function index(PostRepository $repos): Response
     {
         if ($this->getUser()) {
             return $this->render('account/index.html.twig',[
-                'posts'=>$repos->findByUser($this->getUser())
+                'posts'=>$repos->findByUser($this->getUser()),
+                // 'post'=>$post
             ]);
         }else {
             $this->redirectToRoute('app_login');   
         }
     }
 
-    #[Route('/delete-post', name: 'app_delete_post')]
-    public function deletePost(PostRepository $repos){
-        return $this->redirectToRoute('app_account');
-        $repos->findByVisibilite(true);
-    }
+    #[Route('/delete-post/{id}', name: 'app_delete_post')]
+    public function deletePost($id,PostRepository $repos): Response
+    {
+        $post = $repos->find($id);
+        $post->setVisibilite(false);
+        $repos->save($post, true);
+        return $this->redirectToRoute('app_account', [], Response::HTTP_SEE_OTHER);    }
 }
