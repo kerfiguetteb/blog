2ec327aaaa63ca31431b6fa4f768ae81455ca30e
kerfiguetteb <kerfiguetteboutman@gmail.com>
2023-06-20 19:21:17 +0200

    style: ajout du bouton enregistrer

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,11 @@ class PostType extends AbstractType
             ->add('title')
             ->add('introduction')
             ->add('content')
+            ->add('enregistrer', SubmitType::class,[
+                'attr'=>[
+                    'class'=>'btn btn-success'
+                ]
+            ])
         ;
     }
 
